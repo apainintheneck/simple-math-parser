57493e6a8da7a0deca791c02379e8ac999991ee9
apainintheneck <apainintheneck@gmail.com>
2020-04-20 19:54:32 -0700

    Finished StringScanner class.

--- src/StringScanner.java
+++ src/StringScanner.java
@@ -13,6 +13,9 @@ public class StringScanner
    private HashSet<Character> specCharSet = new HashSet(); //Set of special characters
    private boolean skipWhitespace = false;
    
+   /**
+    * Sets delimiter to skip all chars according to the Character.isWhitespace() method.
+    */
    public void skipWhitespace() { skipWhitespace = true; }
    
    public void addDelimiter(char delim) { delimSet.add(delim); }
@@ -21,34 +24,54 @@ public class StringScanner
          delimSet.add(ch);
    }
    
+   /**
+    * Add character that should become individual token when scanned.
+    * @param specialChar 
+    */
    public void addSpecialChar(char specialChar) { specCharSet.add(specialChar); }
+   /**
+    * Add array of characters that should become individual tokens when scanned.
+    * @param specialCharArray
+    */
    public void addSpecialChar(char[] specialCharArray) {
       for(char ch : specialCharArray)
          specCharSet.add(ch); 
    }
    
+   /**
+    * Scans string and breaks it up following the delimiters and special characters.
+    * Remember that delimiters have precedence over special characters. If there are
+    * any characters set as both, they will be interpreted as delimiters.
+    * @param inputStr
+    * @return LinkedList of Strings
+    */
    public LinkedList<String> scan(String inputStr){
+      tokenList.clear();
+      
       char ch;
       String token = "";
       for(int i = 0; i < inputStr.length(); i++) {
          ch = inputStr.charAt(i);
          
-         if(skipWhitespace && Character.isWhitespace(ch)) {
+         if(isDelim(ch)) {
             saveToken(token);
             token = "";
-         } else
-         if(delimSet.contains(ch)) {
+         } else if(specCharSet.contains(ch)) {
             saveToken(token);
             token = "";
+         } else {
+            token = token + ch;
          }
-         
-         if()
             
       }
+      
+      saveToken(token);
+      
+      return tokenList;
    }
    
    private boolean isDelim(char ch) {
-      return (skipWhitespace && Character.isWhitespace(ch)) ? true : delimSet.contains(ch);
+      return (skipWhitespace && Character.isWhitespace(ch)) || delimSet.contains(ch);
    }
    
    private void saveToken(String token) {
