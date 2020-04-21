35df01195029a90839bdf0f1e3cb1a56de46ffbc
apainintheneck <apainintheneck@gmail.com>
2020-04-21 00:07:02 -0700

    Created Factory class for the mathNodes package.

---
+++ src/mathNodes/Factory.java
@@ -0,0 +1,31 @@
+package mathNodes;
+
+public class Factory
+{
+   public Expression buildNode(String token)
+   {
+      if(token.length() == 1)
+      {
+         switch(token.charAt(0))
+         {
+            case '+': return new Add();
+            case '-': return new Sub();
+            case '*': return new Mult();
+            case '/': return new Div();
+            case '^': return new Pow();
+         }
+      }
+      
+      try {
+         int numInt = Integer.parseInt(token);
+         return new Int(numInt);
+      } catch(NumberFormatException e1) {
+         try {
+            double numDub = Double.parseDouble(token);
+            return new Dec(numDub);
+         } catch(NumberFormatException e2) {
+            return null;
+         }
+      }
+   }
+}
