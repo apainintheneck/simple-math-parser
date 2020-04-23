a974be49ae3c83885d01e0162387d8b2a19e7df4
apainintheneck <apainintheneck@gmail.com>
2020-04-23 13:03:10 -0700

    Created CalcApp.java main for the math expression parser.
    It either takes the first argument passed it by the console or it scans
    lines from the command line until it gets an argument. Either way it
    tries to parse it mathematically using the MathTree class.

--- src/CalcApp.java
+++ src/CalcApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This is the main for this calculator app. It features the MathTree class for 
  * parsing and calculation.
@@ -10,7 +12,27 @@ public class CalcApp
    
    public static void main(String[] args)
    {
+      String mathExpression;
       
+      if(args.length < 1)
+      {
+         Scanner in = new Scanner(System.in);
+         String inputStr = "";
+         
+         while(inputStr.isBlank())
+            inputStr = in.nextLine();
+         
+         mathExpression = inputStr;
+         
+         in.close();
+      }
+      else
+      {
+         mathExpression = args[0];
+      }
+      
+      if(calcTree.init(mathExpression))
+         System.out.println(calcTree.solve());
    }
 
 }
